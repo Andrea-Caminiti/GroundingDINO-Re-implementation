de259dedede0b3c78b55f06aa26d59ea5cd5238c
Andrea Caminiti <caminiti.1924053@studenti.uniroma1.it>
2024-06-01 18:12:40 +0200

    Small fix

--- models/DeformableAtt.py
+++ models/DeformableAtt.py
@@ -10,7 +10,7 @@ from torch.autograd.function import once_differentiable
 from torch.nn.init import constant_, xavier_uniform_
 
 try:
-    from groundingdino import _C
+    from models import _C
 except:
     warnings.warn("Failed to load custom C++ ops. Running on CPU mode Only!")
 
