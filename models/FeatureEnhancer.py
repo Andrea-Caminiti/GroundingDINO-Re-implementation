528ec4e2727f214a459cb54b428ee6ad18f9dcdd
Andrea Caminiti <caminiti.1924053@studenti.uniroma1.it>
2024-06-01 18:37:15 +0200

    Integrated backbone into feature enhancer

--- models/FeatureEnhancer.py
+++ models/FeatureEnhancer.py
@@ -17,8 +17,35 @@ class FeatureEnhancerLayer(nn.Module):
             nn.ReLU(),
             nn.Linear(d_model * 4, d_model)
         )
+        self.backbone = MultiModalFeatureExtractor()
+        self.input_proj_list = []
+        self.bert_size = 768
+        self.feat_map = nn.Linear(bert_size, self.d_model, bias=True)
+    def forward(self, images, texts, reference_points):
+        text_features, img_features = backbone(texts, images)
 
-    def forward(self, img_features, text_features, spatial_shapes, reference_points):
+        #Img_features
+        spatial_shapes = []
+        feat_flatten = []
+        for _ in range(len(img_features)): 
+            in_channels = img_features[_].shape[-1]
+            self.input_proj_list.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, self.d_model, kernel_size=1),
+                    nn.GroupNorm(32, self.d_model),
+                )
+            )
+        self.input_proj = nn.ModuleList(self.input_proj_list)
+        for l, feat in enumerate(img_features):
+            B, H, W, C = feat.shape
+            feat_flatten.append(self.input_proj[l](feat.permute(0,3,1,2)).permute(0, 2, 3, 1).flatten(1,2))
+            spatial_shapes.append([H, W])
+        spatial_shapes = torch.tensor(spatial_shapes)
+        feat_flatten = torch.cat(feat_flatten, 1)
+
+        #Text_features
+        text_features = self.feat_map(text_features)
+
         img_features = self.image_self_attn(img_features, img_features, img_features, spatial_shapes= spatial_shapes, reference_points=reference_points) #B x Sl X E
         text_features = self.text_self_attn(text_features, text_features, text_features)[0] #B x Sl X E 
         img_to_text_features = self.img_to_text_cross_attn(text_features, img_features, img_features)[0]
@@ -32,42 +59,12 @@ if __name__ == '__main__':
     device='cpu'
     model = FeatureEnhancerLayer(1024)
     images = torch.randn(2, 3, 224, 224) # Example batch of images
-    texts = ["a photo of a cat", "a picture of a dog Andrea"]  # Example batch of text descriptions
+    texts = ["a photo of a cat", "a picture of a dog"]  # Example batch of text descriptions
     bs = 2
     num_query = 4165
     num_levels = 4
     reference_points = torch.rand(bs, num_query, num_levels, 2)
-    backbone = MultiModalFeatureExtractor()
-    text_features, img_features = backbone(texts, images)
-
-    #Img_features
-    spatial_shapes = []
-    feat_flatten = []
-    num_backbone_outs = 4
-    input_proj_list = []
-    hidden_dim = 1024
-    for _ in range(len(img_features)): 
-        in_channels = img_features[_].shape[-1]
-        input_proj_list.append(
-            nn.Sequential(
-                nn.Conv2d(in_channels, hidden_dim, kernel_size=1),
-                nn.GroupNorm(32, hidden_dim),
-            )
-        )
-    input_proj = nn.ModuleList(input_proj_list)
-    for l, feat in enumerate(img_features):
-        B, H, W, C = feat.shape
-        feat_flatten.append(input_proj[l](feat.permute(0,3,1,2)).permute(0, 2, 3, 1).flatten(1,2))
-        spatial_shapes.append([H, W])
-    spatial_shapes = torch.tensor(spatial_shapes)
-    feat_flatten = torch.cat(feat_flatten, 1)
-
-    #Text_features
-    bert_size = 768
-    feat_map = nn.Linear(bert_size, hidden_dim, bias=True)
-    text_features = feat_map(text_features)
-    print(text_features.shape)
-    #Feature Enhancer
-    img_features, text_features = model(feat_flatten, text_features, spatial_shapes, reference_points)
+    
+    img_features, text_features = model(images, texts, reference_points)
     print(img_features.shape, text_features.shape)
 
